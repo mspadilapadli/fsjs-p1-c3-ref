25a65dc024ed7d4a448b62d866bd6068a15ac0ee
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-16 22:46:33 +0700

    q1 controller

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,6 +1,15 @@
 const Model = require("../Models/model");
 const View = require("../Views/view");
 
-class Controller {}
+class Controller {
+    static async query1() {
+        try {
+            let data = await Model.Q1();
+            View.showQuery(data);
+        } catch (error) {
+            View.showError(error);
+        }
+    }
+}
 
 module.exports = Controller;
