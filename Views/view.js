ec91cc7571a7e2bbac94211f32b8b48257f43d80
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-16 22:47:12 +0700

    q1 view

--- Views/view.js
+++ Views/view.js
@@ -1,3 +1,11 @@
-class View {}
+class View {
+    static showQuery(data) {
+        console.table(data);
+    }
+
+    static showError(error) {
+        console.log(error);
+    }
+}
 
 module.exports = View;
