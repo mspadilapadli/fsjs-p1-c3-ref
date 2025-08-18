ef935980b555423ac7b1e377d50da4fccb1e77b7
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-18 21:29:53 +0700

    factory class : add Menus class

--- Models/class.js
+++ Models/class.js
@@ -1,3 +1,14 @@
+class Menu {
+    constructor(id, name, category, stock, price, createdAt) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.stock = stock;
+        this.price = price;
+        this.createdAt = createdAt;
+    }
+}
+
 class Factory {}
 
 module.exports = Factory;
