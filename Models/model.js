c17999c0f6158ccf714461cd0e35f0d4a7da8dbf
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-19 23:11:44 +0700

    q2 model

--- Models/model.js
+++ Models/model.js
@@ -17,6 +17,21 @@ AND date_part('month',m."createdAt") >=5 AND date_part('month',m."createdAt") <=
             throw error;
         }
     }
+    static async Q2() {
+        try {
+            let query = `SELECT m."id", m."name", m."price", m."stock", c."name" AS "category", m."createdAt" FROM "Menus" m INNER JOIN "Categories" c ON m."categoryId" = c.id
+WHERE  m."stock" = (
+		SELECT max(m."stock") FROM "Menus" m 
+		INNER JOIN "Categories" c ON m."categoryId" = c."id"
+)`;
+            let data = await pool.query(query);
+            let getData = data.rows;
+            let getInstace = Factory.showMenus(getData);
+            return getInstace;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 module.exports = Model;
