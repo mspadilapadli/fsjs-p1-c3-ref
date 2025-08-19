479cbe53aa4ea773ea98e5b6691f1647d561e931
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-19 23:11:22 +0700

    q1 model

--- Models/model.js
+++ Models/model.js
@@ -1,4 +1,22 @@
 const Factory = require("./class");
-class Model {}
+const pool = require("../config");
+class Model {
+    static async Q1() {
+        try {
+            let query = `SELECT m."id", m."name", m."price", m."stock", c."name" AS "category", m."createdAt" FROM "Menus" m INNER JOIN "Categories" c ON m."categoryId" = c.id
+WHERE c."name" = 'Coffee'
+AND date_part('month',m."createdAt") >=5 AND date_part('month',m."createdAt") <= 6`;
+
+            let data = await pool.query(query);
+            let getData = data.rows;
+            let getInstace = Factory.showMenus(getData);
+            // console.log(getInstace);
+
+            return getInstace;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
 
 module.exports = Model;
